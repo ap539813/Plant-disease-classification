d24ab2739f3f581fbad5fc0faa51f55681074b45
Ankush Pandey <ap539813@gmail.com>
2022-01-07 20:31:25 +0530

    Added columns to the layout
    To display the input image and the output class in separate columns I have added two columns.

--- app_classifier.py
+++ app_classifier.py
@@ -50,43 +50,46 @@ def prediction(model_path, class_dict):
     if uploaded_file is not None:
         # image = cv2.imread(uploaded_file)
         image = Image.open(uploaded_file)
-        st.image(image, caption='Input Image.', use_column_width=True)
+        col1, col2 = st.columns(2)
+        col1.image(image, caption='Input Image.', use_column_width=True)
         st.write("")
         image = image.resize((img_width, img_height))
         image = np.array(image).reshape((1, img_width, img_height, 3))/255
 
         
         pred = model.predict(image).argmax()
+
+        
         # st.write(class_dict)
         if class_dict[pred] == "Apple___Apple_scab":
-            st.error("This is not a healthy Apple Plant. It has a de named APPLE SCAB")
-            st.write("To know more about about the disease and it's cure check out this [link](https://arborjet.com/2019/03/28/apple-scab-symptoms-and-how-to-treat)")
+            col2.error("This is not a healthy Apple Plant. It has a de named APPLE SCAB")
+            col2.write("To know more about about the disease and it's cure check out this [link](https://arborjet.com/2019/03/28/apple-scab-symptoms-and-how-to-treat)")
 
         elif class_dict[pred] == "Apple___Black_rot":
-            st.error("This is not a healthy Apple Plant. It has a de named BLACK ROT")
-            st.write("To know more about about the disease and it's cure check out this [link](https://extension.umn.edu/plant-diseases/black-rot-apple)")
+            col2.error("This is not a healthy Apple Plant. It has a de named BLACK ROT")
+            col2.write("To know more about about the disease and it's cure check out this [link](https://extension.umn.edu/plant-diseases/black-rot-apple)")
         
         elif class_dict[pred] == "Apple___Cedar_apple_rust":
-            st.error("This is not a healthy Apple Plant. It has a disease named CEDAR APPLE RUST")
-            st.write("To know more about about the disease and it's cure check out this [link](https://gardenerspath.com/how-to/disease-and-pests/cedar-apple-rust-control/)")
+            col2.error("This is not a healthy Apple Plant. It has a disease named CEDAR APPLE RUST")
+            col2.write("To know more about about the disease and it's cure check out this [link](https://gardenerspath.com/how-to/disease-and-pests/cedar-apple-rust-control/)")
         
         elif class_dict[pred] == "Apple___healthy":
-            st.success("Congrats This is a healthy Apple Plant")
+            col2.success("Congrats This is a healthy Apple Plant")
         
         elif class_dict[pred] == "Grape___Black_rot":
-            st.error("This is not a healthy Grape Plant. It has a de named BLACK ROT")
-            st.write("To know more about about the disease and it's cure check out this [link](https://www.gardeningknowhow.com/edible/fruits/grapes/black-rot-grape-treatment.htm)")
+            col2.error("This is not a healthy Grape Plant. It has a de named BLACK ROT")
+            col2.write("To know more about about the disease and it's cure check out this [link](https://www.gardeningknowhow.com/edible/fruits/grapes/black-rot-grape-treatment.htm)")
         
         elif class_dict[pred] == "Grape___healthy":
-            st.success("Congrats This is a healthy Grape Plant")
+            col2.success("Congrats This is a healthy Grape Plant")
         
         elif class_dict[pred] == "Grape___Esca_":
-            st.error("This is not a healthy Grape Plant. It has a de named ESCA")
-            st.write("To know more about about the disease and it's cure check out this [link](http://ipm.ucanr.edu/PMG/r302100511.html)")
+            col2.error("This is not a healthy Grape Plant. It has a de named ESCA")
+            col2.write("To know more about about the disease and it's cure check out this [link](http://ipm.ucanr.edu/PMG/r302100511.html)")
         
         elif class_dict[pred] == "Grape___Leaf_blight_":
-            st.error("This is not a healthy Grape Plant. It has a de named LEAF BLIGHT")
-            st.write("To know more about about the disease and it's cure check out this [link](https://www.goodfruit.com/11-tips-to-beat-grape-fungal-diseases)")
+            col2.error("This is not a healthy Grape Plant. It has a de named LEAF BLIGHT")
+            col2.write("To know more about about the disease and it's cure check out this [link](https://www.goodfruit.com/11-tips-to-beat-grape-fungal-diseases)")
 
 
 
