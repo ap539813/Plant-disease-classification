6fae11771e6c3dd39bc9862f0f5393ee62fd4471
Ankush Pandey <ap539813@gmail.com>
2022-06-21 14:38:27 +0530

    adde the homepage image_path in variables

--- important_variables.py
+++ important_variables.py
@@ -1,4 +1,5 @@
 from keras import backend as K
+import base64
 
 # Define the hight and width of the images as required
 
@@ -10,3 +11,9 @@ if K.image_data_format() == 'channels_first':
     input_shape = (3, img_width, img_height)
 else:
     input_shape = (img_width, img_height, 3)
+
+"""### gif from local file"""
+file_ = open("Plant disease classification.gif", "rb")
+contents = file_.read()
+data_url = base64.b64encode(contents).decode("utf-8")
+file_.close()
