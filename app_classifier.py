ac885364f92dfa0bceeee2661715c92949690605
Ankush Pandey <ap539813@gmail.com>
2022-06-21 14:38:04 +0530

    added homepage functionality

--- app_classifier.py
+++ app_classifier.py
@@ -3,8 +3,9 @@ import streamlit as st
 import seaborn as sns
 
 # importing the local modules
-from important_variables import input_shape
+from important_variables import input_shape, data_url
 from prediction import prediction
+from homepage import homepage
 
 from PIL import Image
 
@@ -27,6 +28,42 @@ print(st.session_state)
 if 'home_page' not in st.session_state:
     st.session_state['home_page'] = True
 
+def homepage():
+    # if st.session_state['home_page']:
+    # home_image = st.image("Plant disease classification.gif")
+
+    st.markdown(
+    f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+    unsafe_allow_html=True,
+)
+
+    c1, c2, c3 = st.columns([1,3,1])
+    # col2.radio(
+    #         "Mode of Operation",
+    #         ("Search", "Configure"),
+    #     )
+
+    c2.markdown('<div style="text-align: center;"> <h2> Select mode of operation </h2></div>', unsafe_allow_html=True)
+    # col2.markdown('<div style="text-align: center;">Hello World!</div>', unsafe_allow_html=True)
+
+
+    search_page = c2.button('Search')
+    config_page = c2.button('Configure')
+
+    st.session_state['home_page'] = False
+    # print(search_page)
+    
+
+    if search_page:
+        print('going for search!!')
+        st.session_state['mode'] = 'Search'
+        home_image.empty()
+        main()
+    if config_page:
+        print('going for configuration!!')
+        st.session_state['mode'] = 'Config'
+        home_image.empty()
+        main()
 
 def main():
     st.sidebar.title("Control Panel")
@@ -58,5 +95,8 @@ def main():
 
 
 if __name__ == '__main__':
-    
+    if st.session_state['home_page']:
+        homepage()
+    else:
+        main()
 
