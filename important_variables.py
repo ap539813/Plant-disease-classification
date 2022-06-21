777d3d9de820a7b840de3016ae3b994f9b805214
Ankush Pandey <ap539813@gmail.com>
2022-06-21 10:11:32 +0530

    more modification in prediction module

--- important_variables.py
+++ important_variables.py
@@ -1,3 +1,12 @@
+from keras import backend as K
+
 # Define the hight and width of the images as required
 
 img_width, img_height = 128, 128
+
+# dimensions of our images.
+img_width, img_height = 128, 128
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
