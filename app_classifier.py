4917477a1ae979b910865ab53140c5b93f2a58c0
Ankush Pandey <ap539813@gmail.com>
2022-06-21 14:01:19 +0530

    Refactored the code

--- app_classifier.py
+++ app_classifier.py
@@ -9,21 +9,26 @@ from prediction import prediction
 from PIL import Image
 
 
-if __name__ == '__main__':
-    ## Basic setup and app layout
-    st.set_page_config(layout="wide")
+## Basic setup and app layout
+st.set_page_config(layout="wide")
 
-    alt.renderers.set_embed_options(scaleFactor=2)
+alt.renderers.set_embed_options(scaleFactor=2)
 
-    hide_streamlit_style = """
-                <style>
-                #MainMenu {visibility: hidden;}
-                footer {visibility: hidden;}
-                </style>
-                """
-    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+print(st.session_state)
+
+if 'home_page' not in st.session_state:
+    st.session_state['home_page'] = True
 
 
+def main():
     st.sidebar.title("Control Panel")
     left_col, middle_col, right_col = st.columns(3)
     palette = sns.color_palette("bright")
@@ -50,4 +55,8 @@ if __name__ == '__main__':
         class_dict = {0:'Grape___Black_rot', 2:'Grape___healthy',
                 1:'Grape___Esca_', 3:'Grape___Leaf_blight_'}
         prediction('grape_cnn1.h5', class_dict, input_shape)
+
+
+if __name__ == '__main__':
+    
 
