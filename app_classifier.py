f1c551aeb96707d049b903a3535efbe3ca554c4a
Ankush Pandey <ap539813@gmail.com>
2022-06-28 11:21:45 +0530

    Added the new section of general classification

--- app_classifier.py
+++ app_classifier.py
@@ -6,6 +6,7 @@ import seaborn as sns
 from important_variables import input_shape, data_url, css_file_path
 from prediction import prediction_individual_plant
 from add_style import local_css
+import tensorflow as tf
 
 from PIL import Image
 
@@ -68,7 +69,21 @@ def main():
                                 'Grape Plant'))
 
     if type_plant == 'General Disease Detection':
-        st.markdown("# Under Development!!!")
+        st.title(f"{type_plant} illness Classification Interface")
+        class_dict = {0: 'Apple___Apple_scab',
+                1: 'Apple___Black_rot', 2: 'Apple___Cedar_apple_rust', 3: 'Apple___healthy', 4: 'Blueberry___healthy',
+                5: 'Cherry_(including_sour)___Powdery_mildew', 6: 'Cherry_(including_sour)___healthy', 7: 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+                8: 'Corn_(maize)___Common_rust_', 9: 'Corn_(maize)___Northern_Leaf_Blight', 10: 'Corn_(maize)___healthy', 11: 'Grape___Black_rot',
+                12: 'Grape___Esca_(Black_Measles)', 13: 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 14: 'Grape___healthy', 15: 'Orange___Haunglongbing_(Citrus_greening)',
+                16: 'Peach___Bacterial_spot', 17: 'Peach___healthy', 18: 'Pepper,_bell___Bacterial_spot', 19: 'Pepper,_bell___healthy',
+                20: 'Potato___Early_blight', 21: 'Potato___Late_blight', 22: 'Potato___healthy', 23: 'Raspberry___healthy',
+                24: 'Soybean___healthy', 25: 'Squash___Powdery_mildew', 26: 'Strawberry___Leaf_scorch', 27: 'Strawberry___healthy',
+                28: 'Tomato___Bacterial_spot', 29: 'Tomato___Early_blight', 30: 'Tomato___Late_blight', 31: 'Tomato___Leaf_Mold',
+                32: 'Tomato___Septoria_leaf_spot', 33: 'Tomato___Spider_mites Two-spotted_spider_mite', 34: 'Tomato___Target_Spot',
+                35: 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 36: 'Tomato___Tomato_mosaic_virus', 37: 'Tomato___healthy'}
+        model = tf.saved_model.load('my_lenet-2.hdf5')
+        prediction_individual_plant(model, class_dict, input_shape)
+
     if type_plant == 'Apple Plant':
         st.title(f"{type_plant} illness Classification Interface")
         class_dict = {0:'Apple___Apple_scab', 2:'Apple___Cedar_apple_rust',
@@ -80,6 +95,8 @@ def main():
         class_dict = {0:'Grape___Black_rot', 2:'Grape___healthy',
                 1:'Grape___Esca_', 3:'Grape___Leaf_blight_'}
         prediction_individual_plant('grape_cnn1.h5', class_dict, input_shape)
+
+    
 
 
 if __name__ == '__main__':
