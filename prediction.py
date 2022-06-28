c69d4cc9ad78fc3c695890cc1086f98ca27dabfa
Ankush Pandey <ap539813@gmail.com>
2022-06-28 16:32:47 +0530

    Added separate code for general classification

--- prediction.py
+++ prediction.py
@@ -1,4 +1,4 @@
-from create_model import create_model, AlexNet
+from create_model import create_model, AlexNet, lenet_5
 from important_variables import img_height, img_width
 import streamlit as st
 from PIL import Image
@@ -69,34 +69,37 @@ def prediction_general(model_path, class_dict, input_shape):
         
         pred = model.predict(image).argmax()
 
-        
-        # st.write(class_dict)
-        if class_dict[pred] == "Apple___Apple_scab":
-            col2.error("This is not a healthy Apple Plant. It has a de named APPLE SCAB")
-            col2.write("To know more about about the disease and it's cure check out this [link](https://arborjet.com/2019/03/28/apple-scab-symptoms-and-how-to-treat)")
+        col2.error(f"This is not a healthy {class_dict[pred].split('___')[0].replace('_', ' ').upper()} Plant. It has a disease named {class_dict[pred].split('___')[1].replace('_', ' ').upper()}")
+        col2.write("To know more about about the disease and it's cure check out this [link](https://arborjet.com/2019/03/28/apple-scab-symptoms-and-how-to-treat)")
 
-        elif class_dict[pred] == "Apple___Black_rot":
-            col2.error("This is not a healthy Apple Plant. It has a de named BLACK ROT")
-            col2.write("To know more about about the disease and it's cure check out this [link](https://extension.umn.edu/plant-diseases/black-rot-apple)")
         
-        elif class_dict[pred] == "Apple___Cedar_apple_rust":
-            col2.error("This is not a healthy Apple Plant. It has a disease named CEDAR APPLE RUST")
-            col2.write("To know more about about the disease and it's cure check out this [link](https://gardenerspath.com/how-to/disease-and-pests/cedar-apple-rust-control/)")
+        # # st.write(class_dict)
+        # if class_dict[pred] == "Apple___Apple_scab":
+        #     col2.error("This is not a healthy Apple Plant. It has a de named APPLE SCAB")
+        #     col2.write("To know more about about the disease and it's cure check out this [link](https://arborjet.com/2019/03/28/apple-scab-symptoms-and-how-to-treat)")
+
+        # elif class_dict[pred] == "Apple___Black_rot":
+        #     col2.error("This is not a healthy Apple Plant. It has a de named BLACK ROT")
+        #     col2.write("To know more about about the disease and it's cure check out this [link](https://extension.umn.edu/plant-diseases/black-rot-apple)")
         
-        elif class_dict[pred] == "Apple___healthy":
-            col2.success("Congrats This is a healthy Apple Plant")
+        # elif class_dict[pred] == "Apple___Cedar_apple_rust":
+        #     col2.error("This is not a healthy Apple Plant. It has a disease named CEDAR APPLE RUST")
+        #     col2.write("To know more about about the disease and it's cure check out this [link](https://gardenerspath.com/how-to/disease-and-pests/cedar-apple-rust-control/)")
         
-        elif class_dict[pred] == "Grape___Black_rot":
-            col2.error("This is not a healthy Grape Plant. It has a de named BLACK ROT")
-            col2.write("To know more about about the disease and it's cure check out this [link](https://www.gardeningknowhow.com/edible/fruits/grapes/black-rot-grape-treatment.htm)")
+        # elif class_dict[pred] == "Apple___healthy":
+        #     col2.success("Congrats This is a healthy Apple Plant")
         
-        elif class_dict[pred] == "Grape___healthy":
-            col2.success("Congrats This is a healthy Grape Plant")
+        # elif class_dict[pred] == "Grape___Black_rot":
+        #     col2.error("This is not a healthy Grape Plant. It has a de named BLACK ROT")
+        #     col2.write("To know more about about the disease and it's cure check out this [link](https://www.gardeningknowhow.com/edible/fruits/grapes/black-rot-grape-treatment.htm)")
         
-        elif class_dict[pred] == "Grape___Esca_":
-            col2.error("This is not a healthy Grape Plant. It has a de named ESCA")
-            col2.write("To know more about about the disease and it's cure check out this [link](http://ipm.ucanr.edu/PMG/r302100511.html)")
+        # elif class_dict[pred] == "Grape___healthy":
+        #     col2.success("Congrats This is a healthy Grape Plant")
         
-        elif class_dict[pred] == "Grape___Leaf_blight_":
-            col2.error("This is not a healthy Grape Plant. It has a de named LEAF BLIGHT")
-            col2.write("To know more about about the disease and it's cure check out this [link](https://www.goodfruit.com/11-tips-to-beat-grape-fungal-diseases)")
+        # elif class_dict[pred] == "Grape___Esca_":
+        #     col2.error("This is not a healthy Grape Plant. It has a de named ESCA")
+        #     col2.write("To know more about about the disease and it's cure check out this [link](http://ipm.ucanr.edu/PMG/r302100511.html)")
+        
+        # elif class_dict[pred] == "Grape___Leaf_blight_":
+        #     col2.error("This is not a healthy Grape Plant. It has a de named LEAF BLIGHT")
+        #     col2.write("To know more about about the disease and it's cure check out this [link](https://www.goodfruit.com/11-tips-to-beat-grape-fungal-diseases)")
